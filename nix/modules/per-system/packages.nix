e6ebd8c3d81b57434da86b91e662da5e5d347f10
Dauliac <github.rover427@passmail.net>
2025-10-03 09:41:43 +0200

    feat(flake-parts): add some examples

--- nix/modules/per-system/packages.nix
+++ nix/modules/per-system/packages.nix
@@ -29,7 +29,7 @@ in
                 description = "The package to use for skopeo.";
                 default = inputs.nix2container.packages.${system}.skopeo-nix2container;
                 defaultText = lib.literalExpression "inputs.nix2container.packages.\${system}.skopeo-nix2container";
-                example = default;
+                example = defaultText;
               };
               #  TODO move all of these into packages under submodules
               containerStructureTest = mkOption rec {
@@ -37,63 +37,63 @@ in
                 description = "The package to use for container-structure-test.";
                 default = pkgs.container-structure-test;
                 defaultText = lib.literalExpression "pkgs.container-structure-test";
-                example = default;
+                example = defaultText;
               };
               podman = mkOption rec {
                 type = types.package;
                 description = "The package to use for podman.";
                 default = pkgs.podman;
                 defaultText = lib.literalExpression "pkgs.podman";
-                example = default;
+                example = defaultText;
               };
               grype = mkOption rec {
                 type = types.package;
                 description = "The package to use for grype.";
                 default = pkgs.grype;
                 defaultText = lib.literalExpression "pkgs.grype";
-                example = default;
+                example = defaultText;
               };
               syft = mkOption rec {
                 type = types.package;
                 description = "The package to use for syft.";
                 default = pkgs.syft;
                 defaultText = lib.literalExpression "pkgs.syft";
-                example = default;
+                example = defaultText;
               };
               trivy = mkOption rec {
                 type = types.package;
                 description = "The package to use for trivy.";
                 default = pkgs.trivy;
                 defaultText = lib.literalExpression "pkgs.trivy";
-                example = default;
+                example = defaultText;
               };
               dive = mkOption rec {
                 type = types.package;
                 description = "The package to use for dive.";
                 default = pkgs.dive;
                 defaultText = lib.literalExpression "pkgs.dive";
-                example = default;
+                example = defaultText;
               };
               nix2container = mkOption rec {
                 type = types.attrs;
                 description = "The nix2container package.";
                 default = inputs.nix2container.packages.${system}.nix2container;
                 defaultText = lib.literalExpression "inputs.nix2container.packages.\${system}.nix2container";
-                example = default;
+                example = defaultText;
               };
               dgoss = mkOption rec {
                 type = types.package;
                 description = "The package to use for dgoss.";
                 default = pkgs.dgoss;
                 defaultText = lib.literalExpression "pkgs.dgoss";
-                example = default;
+                example = defaultText;
               };
               skaffold = mkOption rec {
                 type = types.package;
                 description = "The package to use for skaffold.";
                 default = pkgs.skaffold;
                 defaultText = lib.literalExpression "pkgs.skaffold";
-                example = default;
+                example = defaultText;
               };
             };
           };
